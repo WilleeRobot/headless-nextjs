9200616c10ad7dd9b990e3f765351d0eb3f40c07
Will L. <will@willlee.dev>
2024-05-13 15:53:20 -0600

    Add some dynamic content and build out cms

--- content/queries.ts
+++ content/queries.ts
@@ -1,4 +1,10 @@
-import { HeroQuery, LogoWallQuery } from "@/types"
+import "server-only"
+import {
+  CustomerPostQuery,
+  HeaderNavQuery,
+  HeroQuery,
+  LogoWallQuery,
+} from "@/types"
 import { contentGraphQLFetcher } from "./fetch"
 
 export const getContentForLogoWall = async () => {
@@ -51,6 +57,102 @@ export const getHeroContent = async () => {
     `
 
   const data = await contentGraphQLFetcher<HeroQuery>({ query })
+
+  if (!data) {
+    throw new Error("Failed to fetch API")
+  }
+
+  return data
+}
+
+export const getContentForHeaderNav = async () => {
+  const query = `#graphql
+    query NavigationCollection($where: NavigationFilter) {
+      navigationCollection(where: $where) {
+        items {
+          label
+          linksCollection {
+            items {
+              label
+              link
+            }
+          }
+        }
+      }
+    }
+  `
+
+  const data = await contentGraphQLFetcher<HeaderNavQuery>({
+    query,
+    variables: {
+      where: {
+        label: "Header",
+      },
+    },
+  })
+
+  if (!data) {
+    throw new Error("Failed to fetch API")
+  }
+
+  return data
+}
+
+export const getContentForCustomerPost = async (slug: string) => {
+  const query = `#graphql
+   query Query($where: CustomerPostFilter) {
+    customerPostCollection(where: $where) {
+      items {
+        title
+        slug
+        customer {
+          logo {
+            url
+            width
+            height
+            title
+          }
+          name
+        }
+        body {
+          json
+        }
+      }
+    }
+  }
+  `
+
+  const data = await contentGraphQLFetcher<CustomerPostQuery>({
+    query,
+    variables: {
+      where: {
+        slug,
+      },
+    },
+  })
+
+  if (!data) {
+    throw new Error("Failed to fetch API")
+  }
+
+  return data
+}
+
+export const getSlugsForPosts = async () => {
+  const query = `#graphql
+      {
+        customerPostCollection {
+        items {
+            slug        
+        }
+        }
+      }
+    `
+  const data = await contentGraphQLFetcher<{
+    customerPostCollection: {
+      items: { slug: string }[]
+    }
+  }>({ query })
 
   if (!data) {
     throw new Error("Failed to fetch API")
--- types.ts
+++ types.ts
@@ -24,3 +24,38 @@ export type LogoWallQuery = {
     }[]
   }
 }
+
+export type HeaderNavQuery = {
+  navigationCollection: {
+    items: {
+      label: string
+      linksCollection: {
+        items: {
+          label: string
+          link: string
+        }[]
+      }
+    }[]
+  }
+}
+
+export type CustomerPostQuery = {
+  customerPostCollection: {
+    items: {
+      title: string
+      slug: string
+      customer: {
+        logo: {
+          url: string
+          width: string
+          height: number
+          title: number
+        }
+        name: string
+      }
+      body: {
+        json: JSON
+      }
+    }[]
+  }
+}
